1b93d468fb7dd8e9f5772bcce50a5e51dace9090
ayasirk <118459590+ayasirk@users.noreply.github.com>
2022-11-30 20:34:37 +0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,17 @@
-public class Hello {
-   public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
-      System.out.println("Abdullah Yasir");
-   }
+public class Main {
+  public static void main(String[] args) {
+
+    // create string variables
+    String str1 = "23";
+    String str2 = "4566";
+
+    // convert string to int
+    // using parseInt()
+    int num1 = Integer.parseInt(str1);
+    int num2 = Integer.parseInt(str2);
+
+    // print int values
+    System.out.println(num1);    // 23
+    System.out.println(num2);    // 4566
+  }
 }
